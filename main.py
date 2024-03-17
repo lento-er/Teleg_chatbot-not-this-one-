f8b0420a9d2468e7a70d4699127265e5d80031c9
xudailin <xudailin_ni@sina.com>
2024-03-17 14:47:07 +0800

    modify main.py

--- main.py
+++ main.py
@@ -11,51 +11,62 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-config = configparser.ConfigParser()
-config.read('config.ini')
+
+class TelegramBot:
+    def __init__(self,config_file = "config.ini"):
+        config = configparser.ConfigParser()
+        config.read(config_file)
+        self.redis1 = redis.Redis(host=config['REDIS']['HOST'], port=config['REDIS']['PORT'], db=0, password=config['REDIS']['PASSWORD'])
+        self.chatgpt = HKBU_ChatGPT(config)
+        self.application = ApplicationBuilder().token(config["TELEGRAM"]["ACCESS_TOKEN"]).build()
+
+    # 下方的async开头的函数是Telegram Bot API规定的函数格式，用于处理用户发送的消息，可以根据实际需求进行新增、改写
+    # update: 用户发送的消息 context: 上下文
+    async def start(self,update: Update, context: ContextTypes.DEFAULT_TYPE):
+        # 定义一个start函数，后面会跟/start命令进行关联，当用户发送/start命令时，使用该函数回复一条消息，信息内容为text
+        # chat_id为用户发送消息的会话ID,text为回复的消息内容
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+    async def caps(self,update: Update, context: ContextTypes.DEFAULT_TYPE):
+        # 课件中的示例函数，后面会跟/caps命令进行关联，将用户发送的消息转换为大写，并发送回去
+        if len(context.args) == 0:
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="Please provide some text to capitalize!")
+            return
+        text_caps = ' '.join(context.args).upper()
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+    async def add(self,update: Update, context: ContextTypes.DEFAULT_TYPE):
+        # 课件中的示例函数，后面会跟/add命令进行关联，使用redis统计命令后面的消息出现了多少次
+        if len(context.args) == 0:
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="Please provide some text to capitalize!")
+            return
+        msg = context.args[0]
+        self.redis1.incr(msg)
+
+        await update.message.reply_text(f'you have said {msg} for {self.redis1.get(msg).decode("UTF-8")} times')
+
+    async def equiped_chatgpt(self,update: Update, context: ContextTypes.DEFAULT_TYPE):
+        # 对接chatgpt的函数，将用户的消息，通过chatgpt接口发送出去，并获取chatgpt的回复，然后发送给用户
+        reply_message = self.chatgpt.submit(update.message.text)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
+
+    def main(self):
+        # 创建一个MessageHandler，处理用户发送的消息，筛选出用户的文本或命令，并将筛选出的内容，交给equiped_chatgpt处理，即让chatgpt进行答复
+        self.application.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), self.equiped_chatgpt))
+        # 创建一个CommandHandler，处理用户发送的命令，当用户发送/start命令时，调用start函数
+        self.application.add_handler(CommandHandler('start', self.start))
+        # 创建一个CommandHandler，处理用户发送的命令，当用户发送/add命令时，调用add函数
+        self.application.add_handler(CommandHandler('add', self.add))
+        # 创建一个CommandHandler，处理用户发送的命令，当用户发送/caps命令时，调用caps函数
+        self.application.add_handler(CommandHandler('caps', self.caps))
+
+        # 启动
+        self.application.run_polling()
 
 
-
-redis1 = redis.Redis(host=config['REDIS']['HOST'], port=config['REDIS']['PORT'], db=0, password=config['REDIS']['PASSWORD'])
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # 当用户发送/start命令时，回复一条消息
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # 当用户发送任何消息时，回复相同的消息
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
-
-async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # 将用户发送的消息转换为大写，并发送回去
-    if len(context.args) == 0:
-        await context.bot.send_message(chat_id=update.effective_chat.id, text="Please provide some text to capitalize!")
-        return
-    text_caps = ' '.join(context.args).upper()
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
-
-async def add(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    msg = context.args[0]
-    redis1.incr(msg)
-    await update.message.reply_text(f'you have said {msg} for {redis1.get(msg).decode("UTF-8")} times')
-
-async def equiped_chatgpt(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    reply_message = chatgpt.submit(update.message.text)
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token(config["TELEGRAM"]["ACCESS_TOKEN"]).build()
-    chatgpt = HKBU_ChatGPT(config)
-    chatgpt_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), equiped_chatgpt)
-    start_handler = CommandHandler('start', start)
-    # 创建一个MessageHandler，处理用户发送的消息，主要过滤用户的文本或命令，并使用echo函数处理消息
-    # echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
-    caps_handler = CommandHandler('caps', caps)
-    add_handler = CommandHandler('add', add)
-    application.add_handler(start_handler)
-    # application.add_handler(echo_handler)
-    application.add_handler(caps_handler)
-    application.add_handler(add_handler)
-    application.add_handler(chatgpt_handler)
+    tel_chatgpt_bot = TelegramBot()
+    tel_chatgpt_bot.main()
 
 
-    application.run_polling()
